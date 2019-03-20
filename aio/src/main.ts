210d47e4fd1b090df799a0ed09825702978770b1
Filipe Silva <filipematossilva@gmail.com>
2019-03-20 17:53:02 +0000

    build: benchmark instructions

--- aio/src/main.ts
+++ aio/src/main.ts
@@ -1,8 +1,8 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import {AppModule} from './app/app.module';
+import {environment} from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +10,14 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule);
 
+// benchmark instructions:
+// start at root and run:
+// - scripts/build-ivy-npm-packages.sh
+// - mv dist/packages-dist-ivy-aot dist/packages-dist
+// - yarn --cwd aio build-with-ivy --progress=false
+// - cd aio
+// - yarn ng serve
+// At this point you can trigger rebuilds by add more lines like these:
+console.log(1);
+console.log(1);
+console.log(1);
